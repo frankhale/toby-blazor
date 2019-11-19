f1fe7dd6234cb6bec57a66482a67b3b9ebc1ab90
Frank Hale <frankhale@gmail.com>
2019-11-19 18:49:58 -0500

    minor changes
    - remove unused usings
    - tweak size of video player
    - change to cyborg bootstrap theme

--- server/Data/DataContext.cs
+++ server/Data/DataContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using TobyBlazor.Models;
 
 namespace TobyBlazor.Data
