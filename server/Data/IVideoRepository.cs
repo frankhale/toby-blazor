3bd87f23b24d8183103c78734d2f80e246b1a0f6
Frank Hale <frankhale@gmail.com>
2019-11-21 22:47:16 -0500

    start implementing the repository pattern

---
+++ server/Data/IVideoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TobyBlazor.Models;
+
+namespace TobyBlazor.Data
+{
+    public interface IVideoRepository
+    {
+        public List<Video> Search(string term);
+        public List<Video> All();
+        public List<Video> Find(string like);
+        public List<Video> FindByGroup(string group);        
+        public void Add(Video v);
+        public void Delete(string ytid);        
+    }
+}
---
+++ server/Data/VideoRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TobyBlazor.Models;
+
+namespace TobyBlazor.Data
+{
+    public class VideoRepository : IVideoRepository
+    {
+        private readonly DataContext db = new DataContext();
+
+        public void Add(Video v)
+        {
+            db.Videos.Add(v);
+            db.SaveChanges();
+        }
+
+        public void Delete(string ytid)
+        {
+            if (String.IsNullOrEmpty(ytid)) return;
+
+            db.Videos.Remove(new Video()
+            {
+                YTId = ytid
+            });
+            db.SaveChanges();            
+        }
+
+        public List<Video> Find(string like)
+        {
+            return db.Videos.Where(x => x.Title.ToLower()
+                            .Contains(like.ToLower()))
+                            .Select(x => x)
+                            .ToList();
+        }
+
+        public List<Video> FindByGroup(string group)
+        {            
+            return db.Videos.Where(x => x.Group.ToLower() == group.ToLower())
+                            .Select(x => x)
+                            .ToList();
+        }
+
+        public List<Video> All()
+        {
+            return db.Videos.ToList()
+                            .Where(x => x.Group.ToLower() != "recently played")
+                            .Select(x => x)
+                            .ToList();
+        }
+
+        public List<Video> Search(string term)
+        {
+            List<Video> results = new List<Video>();
+
+            if (String.IsNullOrEmpty(term.Trim())) return results;
+
+            var value = term.ToLower().Split(new char[] { ' ' }, 2);
+
+            return value switch
+            {
+                _ when value[0] == "/all" => All(),
+                _ when value[0] == "/rp" => FindByGroup("recently played"),
+                _ when value[0] == "/group" && value.Length > 1 && !String.IsNullOrEmpty(value[1]) => FindByGroup(value[1]),                
+                _ => Find(term)
+            };
+        }
+    }
+}
