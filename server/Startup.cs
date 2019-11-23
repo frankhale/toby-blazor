a2faa13f3acbd097105df00d9f6393c872847736
Frank Hale <frankhale@gmail.com>
2019-11-23 18:54:46 -0500

    Oops!
    Accidentally removed a @using

--- server/Startup.cs
+++ server/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TobyBlazor.Data;
 
 namespace TobyBlazor
 {
