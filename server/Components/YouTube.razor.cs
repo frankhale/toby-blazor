d5ac703dc55a125ced2673971f7e0bc2c61d6c39
Frank Hale <frankhale@gmail.com>
2020-08-11 23:43:09 -0400

    Fix JsRuntime name

--- server/Components/YouTube.razor.cs
+++ server/Components/YouTube.razor.cs
@@ -9,7 +9,7 @@ namespace TobyBlazor.Components
     public partial class YouTube : ComponentBase
     {
         [Inject]
-        IJSRuntime jsRuntime { get; set; }
+        IJSRuntime JsRuntime { get; set; }
 
         private Timer _timer;
         private bool playerReady = false;
@@ -23,24 +23,24 @@ namespace TobyBlazor.Components
         {
             if (firstRender)
             {
-                await jsRuntime.InvokeVoidAsync("initializeYouTubePlayer");
+                await JsRuntime.InvokeVoidAsync("initializeYouTubePlayer");
 
                 // Periodically check to see if the api is ready and the player is created
                 // When everything is ready dispose of the timer
                 _timer = new Timer(async (e) =>
                 {
-                    playerReady = await jsRuntime.InvokeAsync<bool>("checkPlayer");
-                    apiReady = await jsRuntime.InvokeAsync<bool>("checkAPIReady");
+                    playerReady = await JsRuntime.InvokeAsync<bool>("checkPlayer");
+                    apiReady = await JsRuntime.InvokeAsync<bool>("checkAPIReady");
 
                     if (apiReady && !playerCreated)
                     {
-                        await jsRuntime.InvokeVoidAsync("createPlayer");
+                        await JsRuntime.InvokeVoidAsync("createPlayer");
                         playerCreated = true;
                     }
 
                     if (playerReady)
                     {
-                        await jsRuntime.InvokeAsync<string>("playVideo", VideoId);
+                        await JsRuntime.InvokeAsync<string>("playVideo", VideoId);
                         _timer.Dispose();
                     }
                 }, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(500));
